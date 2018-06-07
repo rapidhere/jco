ff560940a623c3a9019cc6bbcf69e3d985dccec0
dongwei.dq <dongwei.dq@alibaba-inc.com>
2018-06-07 13:33:53 +0800

    INIT COMMIT

---
+++ jco-core/src/main/java/ranttu/rapid/jco/await.java
@@ -0,0 +1,12 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2018 All Rights Reserved.
+ */
+package ranttu.rapid.jco;
+
+/**
+ * @author rapid
+ * @version $Id: await.java, v 0.1 2018年06月07日 1:22 PM rapid Exp $
+ */
+public @interface await {
+}
---
+++ jco-test/src/test/java/ranntu/rapid/jco/test/Greetings.java
@@ -0,0 +1,20 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2018 All Rights Reserved.
+ */
+package ranntu.rapid.jco.test;
+
+import org.testng.annotations.Test;
+
+/**
+ * the test mark that the test task is start up
+ *
+ * @author rapid
+ * @version $Id: Greetings.java, v 0.1 2018年06月07日 1:27 PM rapid Exp $
+ */
+public class Greetings {
+    @Test
+    public void greeting() {
+        System.out.println("Hello World!");
+    }
+}
